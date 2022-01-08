d56e31664aa2fd9ccd275c58e4f7648675aea7be
akshathkashyap <akshath.kp99@gmail.com>
2022-01-09 00:50:18 +0530

    updated short films

--- src/components/ShortFilms.js
+++ src/components/ShortFilms.js
@@ -9,23 +9,23 @@ function ShortFilms() {
       poster: 'Yaadein_poster',
       link: 'https://drive.google.com/file/d/1TFFyt-cL_JqrY8xf6STB6_gc99H-QUBt/view?usp=sharing',
       genre: 'Independent Student short film, Heartwarming',
-      duration : '2 mins 47secs',
+      duration : '2 mins 47 secs',
       filmmaker : 'Madhusmita Das',
       synopsis : 'This film is based on a very simple life story of us, where we forget to acknowledge the close ones when they are always around us, but when they are not then we always recall them... This is a represenatation of such little things between a mother and son.',
     },
     {
-      name: 'project 2',
-      poster: 'about',
-      link: 'https://www.google.com/',
-      genre: 'Lorem ipsum',
-      duration : '2 mins 47secs',
+      name: 'The Shell',
+      poster: 'The Shell_poster',
+      link: 'https://drive.google.com/file/d/1m9Z43FmMZ7lCvM_QkTI6Mc0Bqiy93mnI/view?usp=sharing',
+      genre: 'Psychological, Fiction, Independent',
+      duration : '6 mins 15 secs',
       filmmaker : 'Madhusmita Das',
-      synopsis : 'This film is based on a very simple life story of us, where we forget to acknowledge the close ones when they are always around us, but when they are not then we always recall them... This is a represenatation of such little things between a mother and son.',
+      synopsis : 'Not being able to be herself to the outer world and becoming the way the world wants to see her, she decides to come out of the shell of whom she is becoming and kill that persona.',
     },
     {
-      name: 'project 3',
-      poster: 'about',
-      link: 'https://www.google.com/',
+      name: 'Locked',
+      poster: 'Locked_poster',
+      link: 'https://drive.google.com/file/d/1o4P8WKxY-GoILVg6m0JldZUn0_pYiSKp/view?usp=sharing',
       genre: 'Lorem ipsum',
       duration : '2 mins 47secs',
       filmmaker : 'Madhusmita Das',
@@ -59,11 +59,14 @@ function ShortFilms() {
                   <td><strong>Filmmaker</strong> <span>:</span></td>
                   <td>{`${project.filmmaker}`}</td>
                 </tr>
+                <tr>
+                  <td><strong>Synopsis</strong> <span>:</span></td>
+                  <td>{`${project.synopsis}`}</td>
+                </tr>
               </tbody>
             </table>
             <button onClick={() => linkHandler(project.link)}><i className='arrow'></i></button>
           </article>
-          <p>{`${project.synopsis}`}</p>
         </section>
       )
     })}
