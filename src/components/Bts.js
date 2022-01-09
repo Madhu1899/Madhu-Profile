a42ae0508b47782d68f7134ff3ab081a4b07d32c
akshathkashyap <akshath.kp99@gmail.com>
2022-01-09 17:25:41 +0530

    added bts album photos

--- src/components/Bts.js
+++ src/components/Bts.js
@@ -5,16 +5,19 @@ import './Bts.css'
 function Bts() {
   const btsProjects = [
     {
-      projectName: 'Amazon Prime Series',
-      projectLink: 'https://drive.google.com/drive/folders/1Cr1xFtKd-tc9zGy4-PXHqO5b6S0cj_aw?usp=sharing',
+      name: 'Amazon Prime Series',
+      source: 'bts_mdLove',
+      link: 'https://drive.google.com/drive/folders/1Cr1xFtKd-tc9zGy4-PXHqO5b6S0cj_aw?usp=sharing',
     },
     {
-      projectName: 'TVC - Green Tea',
-      projectLink: 'https://drive.google.com/drive/folders/1ZUtGaFOoXy4Fnrz8NAYDpyI6skCVn43Y?usp=sharing',
+      name: 'TVC - Green Tea',
+      source: 'bts_tvc',
+      link: 'https://drive.google.com/drive/folders/1ZUtGaFOoXy4Fnrz8NAYDpyI6skCVn43Y?usp=sharing',
     },
     {
-      projectName: 'ZEE5 - Comedy Shots',
-      projectLink: 'https://drive.google.com/drive/folders/1ZVs74Ee152wIyFGWQaFbUipk3dwbeyiT?usp=sharing',
+      name: 'ZEE5 - Comedy Shots',
+      source: 'bts_zee5',
+      link: 'https://drive.google.com/drive/folders/1ZVs74Ee152wIyFGWQaFbUipk3dwbeyiT?usp=sharing',
     },
   ];
 
@@ -26,9 +29,9 @@ function Bts() {
     <section className='bts info'>
     {btsProjects.map((project) => {
       return (
-        <span key={`project${project.projectName}`}>
-          <img className='project' src={ process.env.PUBLIC_URL + `/images/about.jpg` } alt="Madhusmita Das" />
-          <span onClick={() => linkHandler(project.projectLink)}>{`${project.projectName}`}</span>
+        <span key={`project${project.name}`}>
+          <img className='project' src={ process.env.PUBLIC_URL + `/images/${project.source}.jpg` } alt="Madhusmita Das" />
+          <span onClick={() => linkHandler(project.link)}>{`${project.name}`}</span>
         </span>
       )
     })}
