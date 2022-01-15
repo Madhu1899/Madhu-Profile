a8115553537aec0fb7564ff1c4a30c9451ada1c2
akshathkashyap <akshath.kp99@gmail.com>
2022-01-15 22:46:55 +0530

    updated bts and about components

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function About() {
       <section className='work'>
         <header>EXPERIENCE</header>
         <p><strong>Intern AD at</strong><br/>Ankahi Ansuni - Jhaagi Files (TV Program), by Disney+Hotstar | July ( 2021 )</p>
-        <p><strong>Behind the Scene Photo/Videographer at</strong><br/>Comedy Shots , a ZEE5 Original Show | June ( 2019 )<br/>L'Oréal Matrix Hair Transformation | August ( 2019 ) & January ( 2021 )<br/>Modern Love (TV Show) - episode : Cutting Chai, an Amazon Prime Show | December ( 2021 )</p>
+        <p><strong>Behind the Scene Photo/Videographer at</strong><br/>Comedy Shots , a ZEE5 Original Show | June ( 2019 )<br/>L'Oréal Matrix Hair Transformation | August ( 2019 ) & January ( 2021 )<br/>Modern Love (TV Show) - ep : Cutting Chai, an Amazon Prime Show | December ( 2021 )</p>
         <p><strong>Short Filmmaker of</strong><br/>Locked ( 2021 )<br/>Yaadein ( 2021 )<br/>The Shell ( 2021 )</p>
         <p><strong>Theatre Experiences at</strong><br/>Thespo | Stage Manager, Photo/videographer ( 2018-2019 )<br/>Tata Lit Live | an AV ( Audio Visual technician ) , December ( 2019 )</p>
       </section>
--- src/components/Bts.js
+++ src/components/Bts.js
@@ -5,20 +5,22 @@ import './Bts.css'
 function Bts() {
   const btsProjects = [
     {
-      name: 'Modern Love (TV Series) , ep : Cutting Chai',
-      desc: 'an Amazon Prime Show',
+      name: 'Modern Love (TV Series)',
+      desc1: 'ep : Cutting Chai',
+      desc2: 'an Amazon Prime Show',
       source: 'bts_mdLove',
       link: 'https://drive.google.com/drive/folders/1Cr1xFtKd-tc9zGy4-PXHqO5b6S0cj_aw?usp=sharing',
     },
     {
-      name: 'Green Tea , TVC',
-      desc: 'by E02 Films and events',
+      name: 'Green Tea',
+      desc1: 'TVC',
+      desc2: 'by E02 Films and events',
       source: 'bts_tvc',
       link: 'https://drive.google.com/drive/folders/1ZUtGaFOoXy4Fnrz8NAYDpyI6skCVn43Y?usp=sharing',
     },
     {
       name: 'Comedy Shots',
-      desc: 'a ZEE5 Original series',
+      desc2: 'a ZEE5 Original series',
       source: 'bts_zee5',
       link: 'https://drive.google.com/drive/folders/1ZVs74Ee152wIyFGWQaFbUipk3dwbeyiT?usp=sharing',
     },
@@ -34,7 +36,11 @@ function Bts() {
       return (
         <span key={`project${project.name}`}>
           <img className='project' src={ process.env.PUBLIC_URL + `/images/${project.source}.jpg` } alt="Madhusmita Das" />
-          <span onClick={() => linkHandler(project.link)}>{`${project.name}`}<br/>{`${project.desc}`}</span>
+          <span onClick={() => linkHandler(project.link)}>
+            <header className='name'>{project.name}</header>
+            <p className='desc1'>{project.desc1}</p>
+            <p className='desc2'>{project.desc2}</p>
+          </span>
         </span>
       )
     })}
