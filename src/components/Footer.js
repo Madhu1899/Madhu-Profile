c1d8233656fab34e2c0296fa582203b971fa1a2c
akshathkashyap <akshath.kp99@gmail.com>
2022-01-23 18:28:12 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,15 @@ import './Footer.css'
 function Footer() {
   return (
     <footer>
-      <section id='identity'>
+      <section id='photo'>
         <img id='madhu' src={ process.env.PUBLIC_URL + `/images/madhu.jpg` } alt="Madhusmita Das" />
-        <img id='sign' src={ process.env.PUBLIC_URL + `/images/signature.png` } alt="Madhusmita Das" />
       </section>
       <section id='contactInfo'>
-        <p><strong>Contact</strong><br/>+91 7980964157</p>
+        <img id='sign' src={ process.env.PUBLIC_URL + `/images/signature.png` } alt="Madhusmita Das" />
+        <p><strong>WhatsApp</strong><br/>+91 8420150570</p>
+        <p><strong>Phone</strong><br/>+91 7980964157</p>
         <p><strong>Email</strong><br/>madhusmita1802@gmail.com</p>
+        <p><strong>Instagram</strong><br/>@madhusmita._.d</p>
       </section>
     </footer>
   )
